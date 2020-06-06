40d14a085461078bede8f0858a04f7ec02971a2b
jj b <jj@thebabers.at>
2020-06-06 11:32:59 +0100

    fixed timings

--- Classes/screen.py
+++ Classes/screen.py
@@ -5,6 +5,10 @@ from kivy.uix.screenmanager import Screen
 class Screen(Screen):
     def __init__(self, *args, **kwargs):
         super(Screen, self).__init__(*args, **kwargs)
+
+    def on_enter(self, *args, **kwargs):
+        super(Screen, self).__init__(*args, **kwargs)
+
         Clock.schedule_once(lambda x: self.post_init())
 
     def post_init(self):
