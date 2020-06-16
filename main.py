23a2400960529b638d6e3406346748b9d80bd5cd
jj b <jj@thebabers.at>
2020-06-16 17:01:04 +0100

    changesds

--- main.py
+++ main.py
@@ -16,29 +16,34 @@ class SpaceBuilder(App):
         super(SpaceBuilder, self).__init__()
         Window.size = 1000, 500
 
+        self.Globals = None
+
         self.setup()
 
-        Globals.width = Window.width
-        Globals.height = Window.height
+        self.Globals.width = Window.width
+        self.Globals.height = Window.height
 
     def setup(self):
         Logger.info("Application: Starting setup")
 
-        Globals.User_data.save_path = str(os.path.join(str(self.user_data_dir), "user_data.json"))
-        Globals.Settings_data.save_path = str(os.path.join(str(self.user_data_dir), "settings_data.json"))
+        self.Globals = Globals()
+        Logger.info("Application: Globals created")
+
+        self.Globals.User_data.save_path = str(os.path.join(str(self.user_data_dir), "user_data.json"))
+        self.Globals.Settings_data.save_path = str(os.path.join(str(self.user_data_dir), "settings_data.json"))
 
         Logger.info("Application: Finished setup")
 
     def build(self):
         Builder.load_file('kv.kv')
+        Logger.info("Application: Kv loaded")
 
-        return ScreenManager(Globals)
+        return ScreenManager(self.Globals)
 
     def on_stop(self):
         sys.exit()
 
 
 if __name__ == '__main__':
-    Globals = Globals()
-    Logger.info("Application: Globals created")
+
     SpaceBuilder().run()
