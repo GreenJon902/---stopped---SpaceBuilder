ae66452d7fe8a5afb67e7aeae518b26703cd44d2
jj b <jj@thebabers.at>
2020-06-17 20:23:37 +0100

    added BaseScreenManager

---
+++ Classes/BaseScreenManager.py
@@ -0,0 +1,14 @@
+from kivy.uix.widget import Widget
+
+
+class BaseScreenManager(Widget):
+    def __init__(self, widget1, widget2):
+        super(BaseScreenManager, self).__init__()
+        self.widget1 = widget1
+        self.widget2 = widget2
+
+        self.add_widget(widget1)
+
+    def next(self):
+        self.clear_widgets()
+        self.add_widget(self.widget2)
