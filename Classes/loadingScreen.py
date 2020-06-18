44a3753b1847352d1dd484c403ec5096e56f3b5b
jj b <jj@thebabers.at>
2020-06-18 15:21:22 +0100

    added loadingScreen bg

--- Classes/loadingScreen.py
+++ Classes/loadingScreen.py
@@ -2,6 +2,9 @@ from functools import partial
 
 from kivy import Logger
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.graphics import *
+from kivy.core.image import Image
 
 from Classes.screen import Screen
 
@@ -25,6 +28,18 @@ class LoadingScreen(Screen):
         self.bus = list()
         self.app = None
 
+
+        self.size = Window.size
+
+        img = Image("textures/loadingScreenBackground.png").texture
+
+        goOver = (Window.height * (img.width / img.height)) - Window.width
+
+        with self.canvas:
+            Rectangle(pos=(goOver / -2, 0),
+                      size=(Window.height * (img.width /
+                                             img.height), Window.height),
+                      texture=img)
 
 
         Logger.info("Application: Loading Screen setup")
