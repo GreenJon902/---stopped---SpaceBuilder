a2cc5109c150b035eba69d947f6df09b40d9de30
jj b <jj@thebabers.at>
2020-06-20 14:00:32 +0100

    added fade

--- Classes/BaseScreenManager.py
+++ Classes/BaseScreenManager.py
@@ -1,4 +1,5 @@
 from kivy import Logger
+from kivy.animation import Animation
 from kivy.uix.widget import Widget
 
 
@@ -11,7 +12,17 @@ class BaseScreenManager(Widget):
         self.add_widget(widget1)
 
     def next(self):
-        self.clear_widgets()
-        self.add_widget(self.widget2())
+        self.widget2 = self.widget2()
+
+        ani = Animation(opacity=1, duration=0)
+        ani += Animation(opacity=0, duration=1)
+        ani.bind(on_complete=lambda x, x2: self.remove_widget(self.widget1))
+        ani.start(self.widget1)
+
+        self.add_widget(self.widget2)
+
+        ani = Animation(opacity=0, duration=0)
+        ani += Animation(opacity=1, duration=1)
+        ani.start(self.widget2)
 
         Logger.info("Application: Starting App")
