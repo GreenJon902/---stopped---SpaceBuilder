7d3da10ab43ec05d2379ddaaf04ed80b77d288ab
jj b <jj@thebabers.at>
2020-06-26 11:39:54 +0100

    added post init func

--- Classes/screen.py
+++ Classes/screen.py
@@ -6,10 +6,15 @@ class Screen(_Screen):
     def __init__(self, *args, **kwargs):
         super(Screen, self).__init__(*args, **kwargs)
 
+        Clock.schedule_once(lambda x: self.post_init(), 0)
+
     def on_enter(self, *args, **kwargs):
         super(Screen, self).__init__(*args, **kwargs)
 
-        Clock.schedule_once(lambda x: self.post_enter())
+        Clock.schedule_once(lambda x: self.post_enter(), 0)
 
     def post_enter(self):
+        pass
+
+    def post_init(self):
         pass
