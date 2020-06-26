404e2aa5d3f616608f75830e91248e00f6ce35d2
jj b <jj@thebabers.at>
2020-06-26 14:56:47 +0100

    locking it so it only visible

--- Classes/betterScatter.py
+++ Classes/betterScatter.py
@@ -1,3 +1,4 @@
+from kivy.graphics.transformation import Matrix
 from kivy.uix.scatter import Scatter
 
 from Classes.globals import get_Globals
@@ -7,5 +8,36 @@ class BetterScatter(Scatter):
     def __init__(self, *args, **kwargs):
         super(BetterScatter, self).__init__(*args, **kwargs)
 
-        self.scale_min = get_Globals().min_zoom
-        self.scale_max = get_Globals().max_zoom
+        self.Globals = get_Globals()
+
+        self.scale_min = self.Globals.GameSettings.min_zoom
+        self.scale_max = self.Globals.GameSettings.max_zoom
+
+    def transform_with_touch(self, touch):
+        if len(self._touches) == 1:
+            xAdd = touch.pos[0] - self._last_touch_pos[touch][0]
+            yAdd = touch.pos[1] - self._last_touch_pos[touch][1]
+
+            left = self._get_x()
+            bottom = self._get_y()
+            right = self.get_right()
+            top = self.get_top()
+
+            dx = 0
+            dy = 0
+
+
+            if left + xAdd <= 0 and right + xAdd >= self.Globals.width:  # right,  left
+                dx = xAdd
+
+            if bottom + yAdd <= 0 and top + yAdd >= self.Globals.height:  # up, down
+                dy = yAdd
+
+
+            self.apply_transform(Matrix().translate(dx, dy, 0))
+
+            return False
+
+
+        else:
+            return super(BetterScatter, self).transform_with_touch(touch)
