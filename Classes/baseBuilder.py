bc3dd894d16d58d0151cb312b7b0af6e2c3fa61d
jj b <jj@thebabers.at>
2020-06-26 10:18:04 +0100

    added baseBuilder basic draw func

--- Classes/baseBuilder.py
+++ Classes/baseBuilder.py
@@ -1,5 +1,23 @@
+from kivy.graphics import *
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
 
 
 class BaseBuilder(Widget):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(self, BaseBuilder).__init__(*args, **kwargs)
+
+        self.Globals = None
+
+        self.canyon = FloatLayout()
+        self.baseBuilder = FloatLayout()
+        self.defences = FloatLayout()
+
+
+    def draw(self):
+        self.canyon.canvas.clear()
+        self.baseBuilder.canvas.clear()
+        self.defences.canvas.clear()
+
+        with self.canyon.canvas:
+            Rectangle(pos=self.pos, size=self.size, texture=self.Globals.Textures.canyon_background)
