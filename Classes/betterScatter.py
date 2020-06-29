979476b205fdd6e48253db36a209e389d2b96d0c
jj b <jj@thebabers.at>
2020-06-29 09:28:29 +0100

    added next part start so scaling stay inside

--- Classes/betterScatter.py
+++ Classes/betterScatter.py
@@ -13,6 +13,7 @@ class BetterScatter(ScatterPlane):
         self.scale_min = self.Globals.GameSettings.base_builder_min_zoom
         self.scale_max = self.Globals.GameSettings.base_builder_max_zoom
 
+
     def transform_with_touch(self, touch):
         changed = False
 
@@ -28,7 +29,6 @@ class BetterScatter(ScatterPlane):
             dx = 0
             dy = 0
 
-
             if left + xAdd <= 0 and right + xAdd >= self.Globals.width:  # right,  left
                 dx = xAdd
 
@@ -42,8 +42,28 @@ class BetterScatter(ScatterPlane):
 
             self.apply_transform(Matrix().translate(dx, dy, 0))
 
-            return changed
-
 
         else:
-            return super(BetterScatter, self).transform_with_touch(touch)
+
+            changed = super(BetterScatter, self).transform_with_touch(touch)
+
+        return changed
+
+
+    def on_transform_with_touch(self, touch):
+        (x, y), (w, h) = self.bbox
+
+        dx = 0
+        dy = 0
+
+        if x > 0:
+            dx = x * -1
+
+        elif x < self.Globals.width:
+            dx = (x * -1) - self.Globals.width
+
+        if y > 0:
+            dy = y * -1
+
+        elif y < self.Globals.width:
+            dy = (y * -1) * - self.Globals.height
