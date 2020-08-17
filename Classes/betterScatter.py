4557012fa6e9e530788e13d0222abe67f754ecaf
jj b <jj@thebabers.at>
2020-08-17 14:09:18 +0100

    Added scroll wheel for zoom for testing

--- Classes/betterScatter.py
+++ Classes/betterScatter.py
@@ -1,14 +1,45 @@
 from kivy.graphics.transformation import Matrix
+from kivy.properties import BooleanProperty
 from kivy.uix.scatter import ScatterPlane
 
 from Classes.globals import get_Globals
 
 
 class BetterScatter(ScatterPlane):
+    scrollToZoom = BooleanProperty(False)
+
     def __init__(self, *args, **kwargs):
         super(BetterScatter, self).__init__(*args, **kwargs)
 
         self.Globals = get_Globals()
+
+    def on_touch_down(self, touch):
+        if touch.is_mouse_scrolling and self.scrollToZoom:
+
+            if touch.button == 'scrolldown':
+                if self.scale < self.Globals.GameSettings.base_builder_max_zoom:
+
+                    nextScale = self.scale * 1.1
+                    if nextScale < self.Globals.GameSettings.base_builder_max_zoom:
+                        self.scale = nextScale
+
+                    else:
+                        self.scale = self.Globals.GameSettings.base_builder_max_zoom
+
+            elif touch.button == 'scrollup':
+                if self.scale > self.Globals.GameSettings.base_builder_min_zoom:
+
+                    nextScale = self.scale * 0.8
+                    if nextScale > self.Globals.GameSettings.base_builder_min_zoom:
+                        self.scale = nextScale
+
+                    else:
+                        self.scale = self.Globals.GameSettings.base_builder_min_zoom
+
+            self.dispatch("on_transform_with_touch", None)
+
+        else:
+            super(BetterScatter, self).on_touch_down(touch)
 
 
     def on_transform_with_touch(self, touch):
