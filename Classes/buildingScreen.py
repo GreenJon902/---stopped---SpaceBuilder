f1c53a4d6003652eff7c56b022700f7ce75403c5
jj b <jj@thebabers.at>
2020-08-26 15:02:56 +0100

    added loging for the settingsScreen and buildingScreen

--- Classes/buildingScreen.py
+++ Classes/buildingScreen.py
@@ -1,6 +1,14 @@
+from kivy import Logger
+
 from Classes.screen import Screen
 
 
 class BuildingScreen(Screen):
     def __init__(self, *args, **kwargs):
         super(BuildingScreen, self).__init__(*args, **kwargs)
+
+    def post_enter(self):
+        Logger.info("Application: Building Screen entered")
+
+    def post_init(self):
+        Logger.info("Application: Building Screen widgets setup")
--- Classes/settingsScreen.py
+++ Classes/settingsScreen.py
@@ -1,6 +1,14 @@
+from kivy import Logger
+
 from Classes.screen import Screen
 
 
 class SettingsScreen(Screen):
     def __init__(self, *args, **kwargs):
         super(SettingsScreen, self).__init__(*args, **kwargs)
+
+    def post_enter(self):
+        Logger.info("Application: Settings Screen entered")
+
+    def post_init(self):
+        Logger.info("Application: Settings Screen widgets setup")
